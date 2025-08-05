9bfa5504e7427e83f55573f89b87263031e97ec1
RovdH <rodyvdhurk@hotmail.com>
2025-08-05 19:58:50 +0200

    userrepo aangemaakt met findby en existsby jpa methodes

--- src/main/java/nl/helicenter/flightmaster/repository/UserRepository.java
+++ src/main/java/nl/helicenter/flightmaster/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package nl.helicenter.flightmaster.repository;
 
-public class UserRepository {
+import nl.helicenter.flightmaster.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
