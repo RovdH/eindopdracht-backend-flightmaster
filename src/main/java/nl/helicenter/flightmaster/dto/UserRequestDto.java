793d1b73c5d37f5fd918ca7f5d3aab7e3de65f1b
RovdH <rodyvdhurk@hotmail.com>
2025-08-05 19:49:26 +0200

    Initial setup van Spring met vast een entiteit boilerplate opgezet.

--- src/main/java/nl/helicenter/flightmaster/dto/UserRequestDto.java
+++ src/main/java/nl/helicenter/flightmaster/dto/UserRequestDto.java
@@ -1,4 +1,43 @@
 package nl.helicenter.flightmaster.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserRequestDto {
+
+    @NotBlank(message = "Email is verplicht")
+    @Email(message = "Ongeldig e-mailadres")
+    private String email;
+
+    @NotBlank(message = "Wachtwoord is verplicht")
+    private String password;
+
+    @NotBlank(message = "Rol is verplicht")
+    private String role;
+
+    // Getters en setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
