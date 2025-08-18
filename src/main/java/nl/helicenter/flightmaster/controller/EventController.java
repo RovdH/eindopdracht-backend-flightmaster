8f1e6c9e6c71a5ab20d4ba476de1bb6e46b6e00d
RovdH <rodyvdhurk@hotmail.com>
2025-08-18 19:18:25 +0200

    Eventcontroller op basis van helicontroller opgetuigd

---
+++ src/main/java/nl/helicenter/flightmaster/controller/EventController.java
@@ -0,0 +1,37 @@
+package nl.helicenter.flightmaster.controller;
+
+import jakarta.validation.Valid;
+import nl.helicenter.flightmaster.dto.EventRequestDto;
+import nl.helicenter.flightmaster.dto.EventResponseDto;
+import nl.helicenter.flightmaster.service.EventService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @PostMapping
+    public ResponseEntity<EventResponseDto> create(@Valid @RequestBody EventRequestDto dto) {
+        EventResponseDto created = eventService.createEvent(dto);
+        return ResponseEntity.ok(created);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDto>> getAll() {
+        return ResponseEntity.ok(eventService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponseDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(eventService.getById(id));
+    }
+}
