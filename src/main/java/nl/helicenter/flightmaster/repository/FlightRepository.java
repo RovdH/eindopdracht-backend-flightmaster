06d38b05e771cae36f3c1e858d6f6d1a726f87f7
RovdH <rodyvdhurk@hotmail.com>
2025-08-19 20:19:32 +0200

    FlightRepo

--- src/main/java/nl/helicenter/flightmaster/repository/FlightRepository.java
+++ src/main/java/nl/helicenter/flightmaster/repository/FlightRepository.java
@@ -1,4 +1,16 @@
 package nl.helicenter.flightmaster.repository;
 
-public interface FlightRepository {
+import nl.helicenter.flightmaster.model.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+    List<Flight> findByEvent_Id(Long eventId);
+
+    boolean existsByEvent_IdAndFlightNumber(Long eventId, String flightNumber);
+
+    boolean existsByEvent_IdAndHelicopter_IdAndStartTime(Long eventId, Long helicopterId, LocalTime startTime);
 }
