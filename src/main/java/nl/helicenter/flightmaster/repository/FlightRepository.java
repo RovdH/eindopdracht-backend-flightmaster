b1e86983f48b25530aaffd25baaf88576cb87e20
RovdH <rodyvdhurk@hotmail.com>
2025-08-25 16:32:34 +0200

    Flight service setup

--- src/main/java/nl/helicenter/flightmaster/repository/FlightRepository.java
+++ src/main/java/nl/helicenter/flightmaster/repository/FlightRepository.java
@@ -5,12 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
 
     List<Flight> findByEvent_Id(Long eventId);
 
     boolean existsByEvent_IdAndFlightNumber(Long eventId, String flightNumber);
-
     boolean existsByEvent_IdAndHelicopter_IdAndStartTime(Long eventId, Long helicopterId, LocalTime startTime);
+
+    Optional<Flight> findTopByEvent_IdAndHelicopter_IdOrderByStartTimeDesc(Long eventId, Long helicopterId);
+    long countByEvent_Id(Long eventId);
+
 }
