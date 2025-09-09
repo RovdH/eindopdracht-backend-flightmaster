6886bdc38434f4b337b13baa035a5993e3b160d9
RovdH <rodyvdhurk@hotmail.com>
2025-09-09 19:04:54 +0200

    PDF Response DTO

---
+++ src/main/java/nl/helicenter/flightmaster/dto/PdfResponseDto.java
@@ -0,0 +1,60 @@
+package nl.helicenter.flightmaster.dto;
+
+import java.time.Instant;
+
+public class PdfResponseDto {
+    private Long id;
+    private Long eventId;
+    private String fileName;
+    private String fileExtension;
+    private long sizeBytes;
+    private Instant createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileExtension() {
+        return fileExtension;
+    }
+
+    public void setFileExtension(String fileExtension) {
+        this.fileExtension = fileExtension;
+    }
+
+    public long getSizeBytes() {
+        return sizeBytes;
+    }
+
+    public void setSizeBytes(long sizeBytes) {
+        this.sizeBytes = sizeBytes;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+}
